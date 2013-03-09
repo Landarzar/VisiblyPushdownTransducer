ca8480cb7b562a88617d3d12e89ae6f0888457c2
Kai Sauerwald <kai.sauerwald@uni-dortmund.de>
2013-03-09 20:19:28 +0100

    Kleine Änderungen

--- src/Main.hs
+++ src/Main.hs
@@ -29,7 +29,8 @@ main = do
     exit        <- newEmptyMVar
     
     -- "Variablen":
-    atm         <- atomically $ newTVar emptyGuiVpt -- Der Automat
+    automat     <- newTVarIO (Nothing::Maybe (VPT Char Char Int Char))
+    atm         <- newTVarIO emptyGuiVpt -- Der Automat
     selectNode  <- newTVarIO (Nothing :: Maybe Int)
     clickNode   <- newTVarIO (Nothing :: Maybe Int)
     clickStart  <- newTVarIO (Nothing:: Maybe (Double,Double))
@@ -59,7 +60,7 @@ main = do
 
     -- Drawing Area
     drawingArea `widgetAddEvents` [ButtonPressMask,ButtonReleaseMask, ButtonMotionMask] 
-    drawingArea `onExpose` (\_ -> renderScene drawingArea atm selectNode clickNode clickStart clickEnd)
+    drawingArea `onExpose` (\_ -> renderScene drawingArea atm selectNode clickNode clickStart clickEnd automat)
     drawingArea `on` buttonPressEvent $ tryEvent $ drawingAreaPress atm selectNode clickNode clickStart clickEnd xml list
     drawingArea `on` buttonReleaseEvent $ tryEvent $ drawingAreaRelease atm clickNode clickStart clickEnd txtCall txtReturn txtInit
     drawingArea `on` motionNotifyEvent $ tryEvent $ drawingAreaMotion atm drawingArea clickNode clickStart clickEnd
@@ -71,13 +72,26 @@ main = do
       printTime (0::Int)
 
     widgetShowAll window
-    
+
     mainGUI
     killThread timer
     signal <- takeMVar exit
-    exitWith signal 
+    exitWith signal
     
-getTextFromTextView :: TextView -> IO String
+{-
+ - Running
+ -}
+ 
+startRun = id
+      
+resetRun = id
+    
+{- 
+ - Kanten Events
+ -}
+ 
+getTextFromTextView :: TextView 
+                    -> IO String
 getTextFromTextView view = do
        buffer <- textViewGetBuffer view
        sItr   <- textBufferGetStartIter buffer
@@ -85,13 +99,118 @@ getTextFromTextView view = do
        str    <- textIterGetText sItr eItr
        return str
 
-oneSelection :: TreeStore (String) -> TreeSelection ->  IO ()
-oneSelection store select = do
+oneSelection :: forall cl. TreeViewClass cl
+             => TreeStore String 
+             -> TreeStore String 
+             -> TreeSelection
+             -> Button
+             -> cl
+             -> cl
+             -> IO ()
+oneSelection store storeRead select btn treeRead treeWrite  = do
    mItr <- treeSelectionGetSelected select
    when (isJust mItr) $ do
      path <- treeModelGetPath store (fromJust mItr)
      v <- treeStoreGetValue store path
-     putStrLn $ show path ++ "selected " ++ v
+     putStrLn $ " @ " ++ show path ++ show (length v) ++ "selected " ++ v
+     when (length path == 2 ) $ do
+        widgetSetSensitivity btn       True
+        widgetSetSensitivity treeRead  False
+        widgetSetSensitivity treeWrite False
+        when (path!!0 == 0) $ do 
+             widgetSetSensitivity treeRead    True
+             select2 <- treeViewGetSelection  treeRead
+             itr2 <- treeSelectionGetSelected select2
+             when (isJust itr2) $ do
+                path2 <- treeModelGetPath storeRead (fromJust itr2)
+                print path2
+        when (path!!0 == 1) $ widgetSetSensitivity treeWrite True
+     unless (length path == 2 ) $ do
+        widgetSetSensitivity btn       False
+        widgetSetSensitivity treeRead  False
+        rSel <- treeViewGetSelection treeRead
+        treeSelectionUnselectAll rSel
+        widgetSetSensitivity treeWrite False
+        
+getSelected :: TreeStore String -> TreeSelection -> IO (Maybe String)
+getSelected str sel =  do
+     itr <- treeSelectionGetSelected sel
+     if isNothing itr then return Nothing
+       else do
+         path <- treeModelGetPath str (fromJust itr)
+         value <- treeStoreGetValue str path
+         return (Just value)
+
+prepare :: String 
+        -> String
+prepare inp = foldl (\str c -> replace [c] ("\'" ++ [c] ++ "\'")  str) inp "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+-- | Diese Funktion konfiguriert den Tree für das Eingabesymbol
+prepareTreeInput :: forall self. TreeViewClass self 
+                 => self
+                 -> self
+                 -> self
+                 -> Button
+                 -> TreeStore String
+                 -> TreeStore String
+                 -> Tree String
+                 -> Tree String
+                 -> Tree String
+                 -> IO (TreeStore String)
+prepareTreeInput treeInput treeRead treeWrite btnOK strRead strWrite mCall mReturn mInit = do
+    forest <- treeStoreNew [mCall , mReturn , mInit]    
+    col <- treeViewColumnNew
+    treeViewColumnSetTitle col "Symbole"
+    treeViewSetModel treeInput forest
+    treeViewSetHeadersVisible treeInput False
+    renderer <- cellRendererTextNew
+    cellLayoutPackStart col renderer False
+    cellLayoutSetAttributes col renderer forest
+           $ \ind -> [cellText := ind]           
+    treeViewAppendColumn treeInput col
+    tree <- treeViewGetSelection treeInput
+    treeSelectionSetMode tree  SelectionBrowse
+    onSelectionChanged tree (oneSelection forest strRead tree btnOK  treeRead treeWrite )
+    return forest
+    
+    
+prepareTreeRead :: forall self. TreeViewClass self 
+                => self 
+                -> IO (TreeStore [Char])
+prepareTreeRead treeRead  = do
+    forest <- treeStoreNew $ map (\c -> Node [c] []) "abcdeefghijklmnopqrstuvwxyz" 
+    col <- treeViewColumnNew
+    treeViewColumnSetTitle col "Symbole"
+    treeViewSetModel treeRead forest
+    treeViewSetHeadersVisible treeRead False
+    renderer <- cellRendererTextNew
+    cellLayoutPackStart col renderer False
+    cellLayoutSetAttributes col renderer forest
+           $ \ind -> [cellText := ind]           
+    treeViewAppendColumn treeRead col
+    tree <- treeViewGetSelection treeRead
+    treeSelectionSetMode tree  SelectionBrowse
+    --onSelectionChanged tree (oneSelection forest tree btnOK)
+    return forest
+    
+prepareTreeWrite :: forall self. TreeViewClass self 
+                 => self 
+                 -> IO (TreeStore [Char])
+prepareTreeWrite treeWrite = do
+    forest <- treeStoreNew $ map (\c -> Node [c] []) "abcdeefghijklmnopqrstuvwxyz"   
+    col <- treeViewColumnNew
+    treeViewColumnSetTitle col "Symbole"
+    treeViewSetModel treeWrite forest
+    treeViewSetHeadersVisible treeWrite False
+    renderer <- cellRendererTextNew
+    cellLayoutPackStart col renderer False
+    cellLayoutSetAttributes col renderer forest
+           $ \ind -> [cellText := ind]           
+    treeViewAppendColumn treeWrite col
+    tree <- treeViewGetSelection treeWrite
+    treeSelectionSetMode tree  SelectionBrowse
+    --onSelectionChanged tree (oneSelection forest tree btnOK)
+    return forest
 
 edgeDialog :: TVar GUIVPT 
            -> GUIVPT
@@ -103,43 +222,45 @@ edgeDialog :: TVar GUIVPT
            -> IO ()
 edgeDialog tVar vpt p q txtCall txtReturn txtInit = do
     Just xml   <- xmlNew "Kante.glade"   
+    lblName     <- xmlGetWidget xml castToLabel "lblName"
     dialog     <- xmlGetWidget xml castToDialog "kantenDialog"
     treeInput  <- xmlGetWidget xml castToTreeView "treeInput"
+    treeRead   <- xmlGetWidget xml castToTreeView "treeStackRead"
+    treeWrite  <- xmlGetWidget xml castToTreeView "treeStackWrite"
+    btnOK      <- xmlGetWidget xml castToButton "btnOK"
     
-    (strCall::String)   <- do { str <- getTextFromTextView txtCall   ; return . read $ "[" ++ str ++ "]" }
-    (strReturn::String) <- do { str <- getTextFromTextView txtReturn ; return . read $ "[" ++ str ++ "]" }
-    (strInit::String)   <- do { str <- getTextFromTextView txtInit   ; return . read $ "[" ++ str ++ "]" }
+    labelSetText lblName $ "Kante von " ++ show p ++ " zu " ++ show q
+    
+    (strCall::String)   <- do { str <- getTextFromTextView txtCall   ; return . read $ "[" ++ prepare str ++ "]" }
+    (strReturn::String) <- do { str <- getTextFromTextView txtReturn ; return . read $ "[" ++ prepare str ++ "]" }
+    (strInit::String)   <- do { str <- getTextFromTextView txtInit   ; return . read $ "[" ++ prepare str ++ "]" }
     mCall   <- return . Node "Call"   $ map (\c -> Node [c] []) strCall
     mReturn <- return . Node "Return" $ map (\c -> Node [c] []) strReturn
     mInit   <- return . Node "Init"   $ map (\c -> Node [c] []) strInit
-
-    forest <- treeStoreNew [mCall , mReturn , mInit]
     
-    col <- treeViewColumnNew
-    treeViewColumnSetTitle col "Call Symbole"
-
-    treeViewSetModel treeInput forest
-    --treeViewSetHeadersVisible treeInput True
-
-    renderer <- cellRendererTextNew
-    cellLayoutPackStart col renderer False
-    cellLayoutSetAttributes col renderer forest
-           $ \ind -> [cellText := ind]
-           
-    treeViewAppendColumn treeInput col
-
-    tree <- treeViewGetSelection treeInput
-    treeSelectionSetMode tree  SelectionBrowse
-    onSelectionChanged tree (oneSelection forest tree)
+    strRead  <- prepareTreeRead  treeRead
+    strWrite <- prepareTreeWrite treeWrite
+    strInput <- prepareTreeInput treeInput treeRead treeWrite btnOK strRead strWrite mCall mReturn mInit
     
     -----
-     
     widgetShowAll dialog
     diares <- dialogRun dialog
     print diares
-    when (diares == ResponseUser 0) $ return ()
+    when (diares == ResponseUser 0) $ do
+         selInpt <- treeViewGetSelection treeInput
+         selCall <- treeViewGetSelection treeRead
+         selRetu <- treeViewGetSelection treeWrite
+         Just inpSel <- getSelected strInput selInpt
+         callSel <- getSelected strRead selCall
+         retSel <- getSelected strWrite selRetu
+         atomically $ writeTVar tVar ( addEdge vpt p q (head inpSel) (maybeHead callSel) (maybeHead retSel) (Just "Transduce THIS!") ) 
     widgetDestroy dialog
     return ()
+
+maybeHead :: Maybe String -> Maybe Char
+maybeHead Nothing      = Nothing
+maybeHead (Just [])    = Just '+'
+maybeHead (Just (x:_)) = Just x
 
 {-  
  -  Events der DrawingArea
@@ -226,7 +347,7 @@ drawingAreaRelease vvptVar tNode tStart tEnd txtCall txtReturn txtInit = do
           znode <- return $ head (getStatesOnPos vvpt p drawRadius)
           node  <- return $ fromJust mnode 
           if btn == RightButton then do 
-              edgeDialog vvptVar vvpt znode node txtCall txtReturn txtInit
+              edgeDialog vvptVar vvpt node znode txtCall txtReturn txtInit
           else return ()              
           else do
           if btn == RightButton then do
@@ -235,9 +356,9 @@ drawingAreaRelease vvptVar tNode tStart tEnd txtCall txtReturn txtInit = do
             node  <- return $ fromJust mnode 
             atomically $ writeTVar vvptVar (setStatePos vvpt node p)
             else return ()         
-        atomically $ writeTVar tNode $ Nothing
+        atomically $ writeTVar tNode  Nothing
         atomically $ writeTVar tStart Nothing
-        atomically $ writeTVar tEnd Nothing
+        atomically $ writeTVar tEnd   Nothing
            
 drawingAreaMotion :: TVar GUIVPT
                  -> DrawingArea
@@ -259,6 +380,10 @@ onWindowDestroy :: MVar ExitCode -> IO ()
 onWindowDestroy exit = do { 
         print "END"; 
         putMVar exit ExitSuccess; mainQuit } 
+        
+{-
+ - Rendern
+ -}
 
 renderScene ::  DrawingArea 
             -> TVar GUIVPT 
@@ -266,8 +391,9 @@ renderScene ::  DrawingArea
             -> TVar (Maybe Int)
             -> TVar (Maybe (Double, Double))
             -> TVar (Maybe (Double, Double))
+            -> TVar (Maybe (VPT Char Char Int Char))
             -> IO Bool
-renderScene da vvptVar selNode tNode tStart tEnd = do
+renderScene da vvptVar selNode tNode tStart tEnd automat = do
     win   <- widgetGetDrawWindow da
     mnode <- atomically $ readTVar tNode
     mstart<- atomically $ readTVar tStart
@@ -296,10 +422,11 @@ renderScene da vvptVar selNode tNode tStart tEnd = do
 renderConn :: (Double,Double) -> (Double,Double) -> Bool -> Render ()
 renderConn (x,y) (z,w) b = do
           moveTo   (x + sintan (abs(x-z)) (abs(y-w))*signum(z-x)) (y + costan (abs(x-z)) (abs(y-w))*signum(w-y))
-          when b $ do
-            lineTo (z + sintan (abs(x-z)) (abs(y-w))*signum(x-z)) (w + costan (abs(x-z)) (abs(y-w))*signum(y-w))
-          unless b $ do
-            lineTo z w
+          (p,q) <- if b then return ((z + sintan (abs(x-z)) (abs(y-w))*signum(x-z)),(w + costan (abs(x-z)) (abs(y-w))*signum(y-w))) else return (z,w)
+          lineTo p q
+          lineTo (p-sin(330.0/180.0*pi)*20) (q+cos(330.0/180.0*pi)*20) 
+          moveTo p q
+          lineTo (p-sin(30.0/180.0*pi - sinh((x-p)/(y-w)) )*20) (q+cos(30.0/180.0*pi - cosh((x-p)/(y-w)))*20)
           stroke
           closePath
           where sintan,costan :: Double -> Double -> Double
